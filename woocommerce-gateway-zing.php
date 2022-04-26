178dea0d4e53df619253728892f8c33ba6e0ff30
Eugeniu T <eugeniu.rtj@gmail.com>
2022-04-26 16:48:54 +0200

    updates

--- woocommerce-gateway-zing.php
+++ woocommerce-gateway-zing.php
@@ -570,8 +570,6 @@ function init_woocommerce_zing()
 
 							$("button[data-action=show-initial-forms]").html("Use Another Card"); 
 							
-					        var BannerHtml = "<div id=\"banner\"><div id=\"d1\"><img border=\"0\" src=\"' . plugins_url() . '/' . get_plugin_data(__FILE__)['TextDomain'] . '/assets/images/general/3dmcsc.svg\" alt=\"MasterCard SecureCode\"></div><div id=\"d2\"><img border=\"0\" src=\"' . plugins_url() . '/' . get_plugin_data(__FILE__)['TextDomain'] . '/assets/images/general/3dvbv.svg\" alt=\"VerifiedByVISA\"></div></div>";
-						    $("form.wpwl-form-card").find(".wpwl-group-submit").after(BannerHtml);
 						    $(".wpwl-group-cardNumber").after( $(".wpwl-group-cardHolder").detach());
 							var visa = $(".wpwl-brand:first").clone().removeAttr("class").attr("class", "wpwl-brand-card wpwl-brand-custom wpwl-brand-VISA");
 							var master = $(visa).clone().removeClass("wpwl-brand-VISA").addClass("wpwl-brand-MASTER");
@@ -605,14 +603,7 @@ function init_woocommerce_zing()
 						onChangeBrand: function(e){
 							$(".wpwl-brand-custom").css("opacity", "0.2");
 							$(".wpwl-brand-" + e).css("opacity", "5"); 
-						},
-						onBeforeSubmitCard: function(){
-							if ($(".wpwl-control-cardHolder").val()==""){
-								$(".wpwl-control-cardHolder").addClass("wpwl-has-error");
-								$(".wpwl-wrapper-cardHolder").append("<div class=\"wpwl-hint wpwl-hint-cardHolderError\">' .
-						'Cardholder not valid' . '</div>");
-							return false; }
-						return true;}
+						}
 					} </script>';
 					if ($this->operation == 'test') echo '<div class="testmode">' . 'This is the TEST MODE. No money will be charged' . '</div>';
 					echo '<div id="zing_payment_container">';
